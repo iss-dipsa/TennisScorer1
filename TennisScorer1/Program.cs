ef21f338a59113285f1cc1945127e868a33f40e8
unknown <A0132524R@CR3538.iss.nus.edu.sg>
2014-08-13 14:42:22 +0800

    A0132524 online assignment

--- TennisScorer1/Program.cs
+++ TennisScorer1/Program.cs
@@ -22,8 +22,141 @@ namespace TennisScorer1
 
         static void DisplayScore(int a, int b)
         {
-            //Modify this method - use ifs and switches based on the value of a and b
-            Console.WriteLine("____");
+
+            if (a == 0)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("Love all");
+                        break;
+                    case 1:
+                        Console.WriteLine("Love-15");
+                        break;
+                    case 2:
+                        Console.WriteLine("Love-30");
+                        break;
+                    case 3:
+                        Console.WriteLine("Love-40");
+                        break;
+                    case 4:
+                        Console.WriteLine("Game B");
+                        break;
+                    case 5:
+                        Console.WriteLine("N/A");
+                        break;
+                }
+            }
+
+            else if (a == 1)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("15-love");
+                        break;
+                    case 1:
+                        Console.WriteLine("15 all");
+                        break;
+                    case 2:
+                        Console.WriteLine("15-30");
+                        break;
+                    case 3:
+                        Console.WriteLine("15-40");
+                        break;
+                    case 4:
+                        Console.WriteLine("Game B");
+                        break;
+                    case 5:
+                        Console.WriteLine("N/A");
+                        break;
+                }
+            }
+            else if (a == 2)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("30-love");
+                        break;
+                    case 1:
+                        Console.WriteLine("30-15");
+                        break;
+                    case 2:
+                        Console.WriteLine("30 all");
+                        break;
+                    case 3:
+                        Console.WriteLine("30-40");
+                        break;
+                    case 4:
+                        Console.WriteLine("Game B");
+                        break;
+                    case 5:
+                        Console.WriteLine("N/A");
+                        break;
+                }
+            }
+            else if (a == 3)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("40-love");
+                        break;
+                    case 1:
+                        Console.WriteLine("40-15");
+                        break;
+                    case 2:
+                        Console.WriteLine("40-30");
+                        break;
+                    case 3:
+                        Console.WriteLine("Deuce");
+                        break;
+                    case 4:
+                        Console.WriteLine("Advantage B");
+                        break;
+                    case 5:
+                        Console.WriteLine("Game B");
+                        break;
+                }
+            }
+            else if (a == 4)
+            {
+                switch (b)
+                {
+                    case 3:
+                        Console.WriteLine("Advantage A");
+                        break;
+                    case 4:
+                        Console.WriteLine("Deuce");
+                        break;
+                    case 5:
+                        Console.WriteLine("Advantage B");
+                        break;
+                    default:
+                        Console.WriteLine("Game A");
+                        break;
+                }
+            }
+            else
+            {
+                switch (b)
+                {
+                    case 3:
+                        Console.WriteLine("Game A");
+                        break;
+                    case 4:
+                        Console.WriteLine("Advantage A");
+                        break;
+                    case 5:
+                        Console.WriteLine("Deuce");
+                        break;
+                    default:
+                        Console.WriteLine("N/A");
+                        break;
+                }
+            }
         }
+       
     }
 }
