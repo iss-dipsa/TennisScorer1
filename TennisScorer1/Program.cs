82c47435b8b28c4623a4e78a80beb9fb4cc0d29b
unknown <A0132515R@CR3524.iss.nus.edu.sg>
2014-08-13 17:08:04 +0800

    Initial commit

--- TennisScorer1/Program.cs
+++ TennisScorer1/Program.cs
@@ -23,7 +23,153 @@ namespace TennisScorer1
         static void DisplayScore(int a, int b)
         {
             //Modify this method - use ifs and switches based on the value of a and b
-            Console.WriteLine("____");
+            Console.Write(" ");
+            if (a == 0 && b == 0)
+            {
+                Console.WriteLine("Love all");
+            }
+            else if (a == 0 && b == 1)
+            {
+                Console.WriteLine("Love-15");
+            }
+            else if (a == 0 && b == 2)
+            {
+                Console.WriteLine("Love-30");
+            }
+            else if (a == 0 && b == 3)
+            {
+                Console.WriteLine("Love-40");
+            }
+            else if (a == 0 && b == 4)
+            {
+                Console.WriteLine("Game B");
+            }
+            else if (a == 0 && b == 5)
+            {
+                Console.WriteLine("N/A");
+            }
+            else if (a == 1 && b == 0)
+            {
+                Console.WriteLine("15-love");
+            }
+            else if (a == 1 && b == 1)
+            {
+                Console.WriteLine("15 all");
+            }
+            else if (a == 1 && b == 2)
+            {
+                Console.WriteLine("15-30");
+            }
+                else if (a == 1 && b == 3)
+            {
+                Console.WriteLine("15-40");
+            }
+            else if (a == 1 && b == 4)
+            {
+                Console.WriteLine("Game B");
+            }
+            else if (a == 1 && b == 5)
+            {
+                Console.WriteLine("N/A");
+            }
+            else if (a == 2 && b == 0)
+            {
+                Console.WriteLine("30-love");
+            }
+            else if (a == 2 && b == 1)
+            {
+                Console.WriteLine("30-15");
+            }
+            else if (a == 2 && b == 2)
+            {
+                Console.WriteLine("30 all");
+            }
+            if (a == 2 && b == 3)
+            {
+                Console.WriteLine("30-40");
+            }
+            else if (a == 2 && b == 4)
+            {
+                Console.WriteLine("Game B");
+            }
+            else if (a == 2 && b == 5)
+            {
+                Console.WriteLine("N/A");
+            }
+            else if (a == 3 && b == 0)
+            {
+                Console.WriteLine("40-love");
+            }
+            else if (a == 3 && b == 1)
+            {
+                Console.WriteLine("40-15");
+            }
+            else if (a == 3 && b == 2)
+            {
+                Console.WriteLine("40-30");
+            }
+            else if (a == 3 && b == 3)
+            {
+                Console.WriteLine("Deuce");
+            }
+            else if (a == 3 && b == 4)
+            {
+                Console.WriteLine("Advantage B");
+            }
+            else if (a == 3 && b == 5)
+            {
+                Console.WriteLine("Game B");
+            }
+                else if (a == 4 && b == 0)
+            {
+                Console.WriteLine("Game A");
+            }
+            else if (a == 4 && b == 1)
+            {
+                Console.WriteLine("Game A");
+            }
+            else if (a == 4 && b == 2)
+            {
+                Console.WriteLine("Game A");
+            }
+            else if (a == 4 && b == 3)
+            {
+                Console.WriteLine("Advantage A");
+            }
+            else if (a == 4 && b == 4)
+            {
+                Console.WriteLine("Deuce");
+            }
+            else if (a == 4 && b == 5)
+            {
+                Console.WriteLine("Advantage B");
+            }
+             else if (a == 5 && b == 0)
+            {
+                Console.WriteLine("N/A");
+            }
+                else if (a == 5 && b == 1)
+            {
+                Console.WriteLine("N/A");
+            }
+            else if (a == 5 && b == 2)
+            {
+                Console.WriteLine("N/A");
+            }
+            else if (a == 5 && b == 3)
+            {
+                Console.WriteLine("Game A");
+            }
+            else if (a == 5 && b == 4)
+            {
+                Console.WriteLine("Advantage A");
+            }
+            else if (a == 5 && b == 5)
+            {
+                Console.WriteLine("Deuce");
+            }
+            
+
         }
     }
 }
