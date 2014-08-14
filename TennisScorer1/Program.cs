a5d72718d9f21bd00f1c73339310bcb2fcb4855c
a0129165 <a0129165@u.nus.edu>
2014-08-14 10:01:26 +0800

    it's not perfect,but it is working.

--- TennisScorer1/Program.cs
+++ TennisScorer1/Program.cs
@@ -22,8 +22,75 @@ namespace TennisScorer1
 
         static void DisplayScore(int a, int b)
         {
-            //Modify this method - use ifs and switches based on the value of a and b
-            Console.WriteLine("____");
+            //define the score calling
+            string c = "Love";
+            string d = "all";
+            string e = "15";
+            string f = "30";
+            string g = "40";
+            string h = "N/A";
+            string i = "Game";
+            string j = "B";
+            string k = "A";
+            string l = "Deuce";
+            string m = "Advantage";
+            //display score calling when A and B have same value
+            if (a == b)
+                if (a == 0 && b == 0)
+                    Console.WriteLine("{0} {1}", c, d);
+                else if (a == 2 && b == 2)
+                    Console.WriteLine("{0} {1}", f, d);
+                else if (a == 1 && b == 1)
+                    Console.WriteLine("{0} {1}", e, d);
+                else if (a >= 3 && b >= 3)
+                    Console.WriteLine("{0}", l);
+            //display score calling when A is not equall to B
+            if (a != b)
+                if (a <= 2 && b == 5)
+                    Console.WriteLine("{0}", h);
+                else if (a == 5 && b <= 2)
+                    Console.WriteLine("{0}", h);
+                else if (a <= 2 && b == 4)
+                    Console.WriteLine("{0} {1}", i, j);
+                else if (a == 3 && b == 5)
+                    Console.WriteLine("{0} {1}", i, j);
+                else if (a == 4 && b <= 2)
+                    Console.WriteLine("{0} {1}", i, k);
+                else if (a == 5 && b == 3)
+                    Console.WriteLine("{0} {1}", i, k);
+                else if ((a == 3 || a == 4) && b == a + 1)
+                    Console.WriteLine("{0} {1}", m, j);
+                else if (a == b + 1 && (b == 3 || b == 4))
+                    Console.WriteLine("{0} {1}", m, k);
+            // the rest  
+            if (a == 0)
+                if (b == 1)
+                    Console.WriteLine("{0}-{1}", c, e);
+                else if (b == 2)
+                    Console.WriteLine("{0}-{1}", c, f);
+                else if (b == 3)
+                    Console.WriteLine("{0}-{1}", c, g);
+            if (a == 1)
+                if (b == 0)
+                    Console.WriteLine("{0}-{1}", e, c);
+                else if (b == 2)
+                    Console.WriteLine("{0}-{1}", e, f);
+                else if (b == 3)
+                    Console.WriteLine("{0}-{1}", e, g);
+            if (a == 2)
+                if (b == 0)
+                    Console.WriteLine("{0}-{1}", f, c);
+                else if (b == 1)
+                    Console.WriteLine("{0}-{1}", f, e);
+                else if (b == 3)
+                    Console.WriteLine("{0}-{1}", f, g);
+            if (a == 3)
+                if (b == 0)
+                    Console.WriteLine("{0}-{1}", g, c);
+                else if (b == 1)
+                    Console.WriteLine("{0}-{1}", g, e);
+                else if (b == 2)
+                    Console.WriteLine("{0}-{1}", g, f);
         }
     }
 }
