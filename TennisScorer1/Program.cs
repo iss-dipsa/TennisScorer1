9ae81c0e5cb85a584b25ea19a8ed884c8bf1c4e4
unknown <a0132522w@CR3559.iss.nus.edu.sg>
2014-08-14 15:42:04 +0800

    A0132522W TennisScorer1

--- TennisScorer1/Program.cs
+++ TennisScorer1/Program.cs
@@ -23,7 +23,117 @@ namespace TennisScorer1
         static void DisplayScore(int a, int b)
         {
             //Modify this method - use ifs and switches based on the value of a and b
-            Console.WriteLine("____");
+           // Console.WriteLine("____");
+
+            if (a == 0)
+            {
+                switch (b) 
+                { 
+                    case 0:
+                        Console.WriteLine("Love all");break;
+                    case 1:
+                        Console.WriteLine("love-15");break;
+                    case 2:
+                        Console.WriteLine("love-30");break;
+                    case 3:
+                        Console.WriteLine("love-40");break;
+                    case 4:
+                        Console.WriteLine("Game B");break;
+                    case 5:
+                        Console.WriteLine("N/A");break;
+                
+                }
+            }
+            else if (a==1)
+            {
+                switch (b) 
+                {
+                    case 0:
+                        Console.WriteLine("15 - love"); break;
+                    case 1:
+                        Console.WriteLine("15 all"); break;
+                    case 2:
+                        Console.WriteLine("15 - 30"); break;
+                    case 3:
+                        Console.WriteLine("15 - 40"); break;
+                    case 4:
+                        Console.WriteLine("Game B"); break;
+                    case 5:
+                        Console.WriteLine("N/A"); break;
+                }
+            }
+            else if (a == 2)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("30 - love"); break;
+                    case 1:
+                        Console.WriteLine("30 all"); break;
+                    case 2:
+                        Console.WriteLine("30 - 30"); break;
+                    case 3:
+                        Console.WriteLine("30 - 40"); break;
+                    case 4:
+                        Console.WriteLine("Game B"); break;
+                    case 5:
+                        Console.WriteLine("N/A"); break;
+                }
+            }
+            else if (a == 3)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("40 - love"); break;
+                    case 1:
+                        Console.WriteLine("40 - 15"); break;
+                    case 2:
+                        Console.WriteLine("40 - 30"); break;
+                    case 3:
+                        Console.WriteLine("Deuce"); break;
+                    case 4:
+                        Console.WriteLine("Advantage B"); break;
+                    case 5:
+                        Console.WriteLine("Game B"); break;
+                }
+            }
+            else if (a == 4)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("Game A"); break;
+                    case 1:
+                        Console.WriteLine("Game A"); break;
+                    case 2:
+                        Console.WriteLine("Game A"); break;
+                    case 3:
+                        Console.WriteLine("Advantage A"); break;
+                    case 4:
+                        Console.WriteLine("Deuce"); break;
+                    case 5:
+                        Console.WriteLine("Advantage B"); break;
+                }
+            }
+            else if (a == 5)
+            {
+                switch (b)
+                {
+                    case 0:
+                        Console.WriteLine("N/A"); break;
+                    case 1:
+                        Console.WriteLine("N/A"); break;
+                    case 2:
+                        Console.WriteLine("N/A"); break;
+                    case 3:
+                        Console.WriteLine("Game A"); break;
+                    case 4:
+                        Console.WriteLine("Advantage A"); break;
+                    case 5:
+                        Console.WriteLine("Deuce"); break;
+                }
+            } cddd
         }
     }
 }
