cc26160fc973f5ebc74c7202c1501787a4a1eef9
parthiba <uparthiban@outlook.com>
2014-08-14 10:39:32 +0800

    ALL COMPLETED

--- TennisScorer1/Program.cs
+++ TennisScorer1/Program.cs
@@ -9,6 +9,9 @@ namespace TennisScorer1
     {
         static void Main(string[] args)
         {
+
+
+
             for (int scoreA = 0; scoreA <= 5; scoreA++)
             {
                 for (int scoreB = 0; scoreB <= 5; scoreB++)
@@ -25,56 +28,99 @@ namespace TennisScorer1
             //Modify this method - use ifs and switches based on the value of a and b
             //Console.WriteLine("____");
 
-            if ((a==b) )
+            string[] value = new string[4] { "love", "15", "30", "40" };
+            if ((a == b))
             {
-                if (a>=3)
+                if (a >= 3)
                 {
                     Console.WriteLine("Deuce \n");
                 }
-                if(a==2)
+                if (a == 2)
                 {
                     Console.WriteLine("30 all \n");
 
                 }
-                if(a==1)
+                if (a == 1)
                 {
                     Console.WriteLine("15 all \n");
                 }
-                if(a==0)
+                if (a == 0)
                 {
                     Console.WriteLine("Love all \n");
                 }
             }
+
             if ((a >= 3) || (b >= 3))
             {
-                if (((a - b) > 2) && (a > b) &&((a==4) || (b==4)))
-                    Console.WriteLine("Game A \n");
-                else if ((b - a) > 2 && (b > a) && ((a==4) || (b==4)))
-                    Console.WriteLine("Game B \n");
+
+                //    Console.WriteLine("Game A \n");
+                //else if ((b - a) > 2 && (b > a) && ((a==4) || (b==4)))
+                //    Console.WriteLine("Game B \n");
 
                 if (((a == 5) || (b == 5)) && ((Math.Abs(a - b)) >= 3))
                     Console.WriteLine("N/A \n");
 
 
-                if ((a > 3) || (b > 3))
+                else if ((a == 4) || (b == 4))
                 {
-                    if ((a > b) && ((a - b) == 1))
+                    if ((a - b) == 1)
                     {
                         Console.WriteLine("Advantage A \n");
                     }
-                    else if ((b > a) && ((b - a) == 1))
+                    else if ((b - a) == 1)
                     {
                         Console.WriteLine("Advantage B \n");
                     }
+
+                    else if (((a >= 4) || (b >= 4)) && Math.Abs(a - b) >= 2)
+                    {
+                        if (a - b >= 2)
+                            Console.WriteLine("Game A");
+                        else if (b - a >= 2)
+                            Console.WriteLine("Game B");
+
+                    }
+
                 }
+
+                else if (((a >= 4) || (b >= 4)) && Math.Abs(a - b) >= 2)
+                {
+                    if (a - b >= 2)
+                        Console.WriteLine("Game A");
+                    else if (b - a >= 2)
+                        Console.WriteLine("Game B");
+
+                }
+
+
+
+
+
+
+            }
+
+
+            if ((a <= 3 && b <= 3) && (a != b))
+            {
+
+
+
+
+                Console.WriteLine(value[a] + "-" + value[b]);
+
             }
 
 
 
-      
 
-            
-          // Console.WriteLine("____");
+
+
+
+
+
+
+
+            // Console.WriteLine("____");
         }
     }
 }
